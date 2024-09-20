db7a362ec098771c5ce10f3ffecf1d7589e817e7
PeshoM <peshooomanov@gmail.com>
2024-09-20 12:50:42 +0300

    init

---
+++ server/server.ts
@@ -0,0 +1,11 @@
+import express, { Express } from 'express';
+import cors from 'cors';
+
+const app: Express = express();
+const port = process.env.PORT || 8000;
+
+app.use(cors({ origin: '*'}));
+
+app.listen(port, () => {
+  console.log('server is on', port);
+});
