44d3cdcdd3d386acc8122aa135cba6e34bac02c3
PeshoM <peshooomanov@gmail.com>
2024-10-06 11:35:37 +0300

    server: ws init

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,33 @@
 import express, { Express } from 'express';
 import cors from 'cors';
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 const app: Express = express();
 const port = process.env.PORT || 8000;
 
-app.use(cors({ origin: "*"}));
+const httpServer = createServer();
+app.use(cors({ origin: "*" }));
 
-app.listen(port, () => {
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*",
+    }
+});
+
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('board', (updatedBoard) => {
+        socket.emit('board', updatedBoard);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+httpServer.listen(port, () => {
     console.log("server is running on", port);
 });
